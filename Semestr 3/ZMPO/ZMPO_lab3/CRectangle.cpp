0665d1c10b149cc3e69410cd424f72008cfa5375
chilczukg <chilczukg@gmail.com>
2016-11-29 00:48:40 +0100

    IT WORKS!

--- Semestr 3/ZMPO/ZMPO_lab3/CRectangle.cpp
+++ Semestr 3/ZMPO/ZMPO_lab3/CRectangle.cpp
@@ -45,41 +45,11 @@ CRectangle &CRectangle::operator=(CRectangle &pcOther) {
 }
 
 CRectangle &CRectangle::operator+(CRectangle &pcOther) {
-    double *minmax = dtab_minmax(*this, pcOther);
-    CRectangle *pc_tmp = new CRectangle(minmax[0], minmax[1], minmax[2], minmax[3]);
+    double *minmax = dtab_minmax(*this, pcOther);CRectangle *pc_tmp = new CRectangle(minmax[0], minmax[1], minmax[2], minmax[3]);
     pc_tmp->b_tmp = true;
     if (pcOther.b_tmp) delete &pcOther;
     delete[] minmax;
     return *pc_tmp;
-}
-
-double CRectangle::d_min_x() {
-    std::cout << "d_min_x" << min(pc_point_one->dGetX(), pc_point_two->dGetX()) << std::endl;
-    return min(pc_point_one->dGetX(), pc_point_two->dGetX());
-}
-
-double CRectangle::d_min_y() {
-    std::cout << "d_min_y" << min(pc_point_one->dGetY(), pc_point_two->dGetY()) << std::endl;
-    return min(pc_point_one->dGetY(), pc_point_two->dGetY());
-}
-
-double CRectangle::d_max_x() {
-    std::cout << "d_max_xy" << max(pc_point_one->dGetX(), pc_point_two->dGetX()) << std::endl;
-    return max(pc_point_one->dGetX(), pc_point_two->dGetX());
-}
-
-double CRectangle::d_max_y() {
-    std::cout << "d_max_y" << max(pc_point_one->dGetY(), pc_point_two->dGetY()) << std::endl;
-    return max(pc_point_one->dGetY(), pc_point_two->dGetY());
-}
-
-double *CRectangle::dtab_minmax(CRectangle &pcOne, CRectangle &pcTwo) {
-    double *minmax = new double[4];
-    minmax[0] = min(pcOne.d_min_x(), pcTwo.d_min_x());
-    minmax[1] = min(pcOne.d_min_y(), pcTwo.d_min_y());
-    minmax[2] = min(pcOne.d_max_x(), pcTwo.d_max_x());
-    minmax[3] = min(pcOne.d_max_y(), pcTwo.d_max_y());
-    return minmax;
 }
 
 CRectangle &CRectangle::operator+(CPoint2D &pcPoint) {
@@ -89,6 +59,32 @@ CRectangle &CRectangle::operator+(CPoint2D &pcPoint) {
     CRectangle *pc_tmp = new CRectangle(minx, miny, maxx, maxy);
     pc_tmp->b_tmp = true;
     return *pc_tmp;
+}
+
+
+double CRectangle::d_min_x() {
+    return min(pc_point_one->dGetX(), pc_point_two->dGetX());
+}
+
+double CRectangle::d_min_y() {
+    return min(pc_point_one->dGetY(), pc_point_two->dGetY());
+}
+
+double CRectangle::d_max_x() {
+    return max(pc_point_one->dGetX(), pc_point_two->dGetX());
+}
+
+double CRectangle::d_max_y() {
+    return max(pc_point_one->dGetY(), pc_point_two->dGetY());
+}
+
+double *CRectangle::dtab_minmax(CRectangle &pcOne, CRectangle &pcTwo) {
+    double *minmax = new double[4];
+    minmax[0] = min(pcOne.d_min_x(), pcTwo.d_min_x());
+    minmax[1] = min(pcOne.d_min_y(), pcTwo.d_min_y());
+    minmax[2] = max(pcOne.d_max_x(), pcTwo.d_max_x());
+    minmax[3] = max(pcOne.d_max_y(), pcTwo.d_max_y());
+    return minmax;
 }
 
 
